824648684a41262fd2fea59203c1b9c11d65b854
VEdant732 <VedantAjay377@gmail.com>
2024-09-16 21:31:52 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function turnon(){
+    document.getElementById("bulbimage2").src="https://rukminim2.flixcart.com/image/850/1000/k0sgl8w0/bulb/y/t/t/g80-edison-filament-bulb-lightwale-original-imafkhse2gbxzwpz.jpeg?q=20&crop=false";
+    document.getElementById("h2").style.backgroundColor="blue"
+}
+function turnoff(){
+    document.getElementById("bulbimage2").src="https://cdn1.byjus.com/wp-content/uploads/2021/05/word-image7.png";
+    document.getElementById("h3").style.backgroundColor="green"
+    
+}
+function nocolor(){
+    document.getElementById("h3").style.backgroundColor="red"
+     document.getElementById("h2").style.backgroundColor="red"
+    
+}
